c446c4021a0f08c24e5c9c9bc36d15becb3630af
Артем <van@vrn.str>
2025-01-31 10:35:13 +0300

    1. Update CI. Change inline script to file

---
+++ pack.sh
@@ -0,0 +1,8 @@
+﻿for file in $(find ./src/ -maxdepth 2 -name '*.csproj') \
+do \
+  dotnet pack $file \
+    --configuration Release \
+    -p:PackageVersion="7.0.0-preview" \
+    -p:RepositoryUrl=https://github.com/t1moH1ch/openiddict-core \
+    -o artifacts/packages/Release/GitHub; \
+done
