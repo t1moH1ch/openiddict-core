1e3f5b9e31889776eb952b451f640cb8016f66af
Артем <van@vrn.str>
2025-02-03 12:36:16 +0300

    1. Update CI. Add build Id

--- pack.sh
+++ pack.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+date_hours=$(date +"%H")
+hours=$(expr $date_hours \* 4)
+quarterHours=$(expr $hours / 15)
+revision=$(expr $hours + $quarterHours + 1)
+random=$((10000 + $RANDOM % 99999))
+buildId=$(date +"%q").${random}.${revision}
+
 for file in $(find ./src/ -maxdepth 2 -name '*.csproj')
 do
   dotnet pack $file \
     --configuration Release \
-    -p:PackageVersion="7.0.0-preview" \
+    -p:PackageVersion="7.0.0-preview.$buildId" \
     -p:RepositoryUrl=https://github.com/t1moH1ch/openiddict-core \
     -o artifacts/packages/Release/GitHub; \
 done
